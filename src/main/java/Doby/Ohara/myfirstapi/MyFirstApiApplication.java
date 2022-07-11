4238ff839b99b81b8c6e4a111edf433e0d28d66a
Tory Doby <torydoby43@gmail.com>
2022-07-11 14:44:24 -0400

    updates

---
+++ src/main/java/Doby/Ohara/myfirstapi/MyFirstApiApplication.java
@@ -0,0 +1,13 @@
+package Doby.Ohara.myfirstapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Doby/Ohara/myfirstapi/MyFirstApiApplicationTests.java
@@ -0,0 +1,13 @@
+package Doby.Ohara.myfirstapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
